8d1f5b5e3c9df8541345fcd27b44974c5c390907
goldstardev2002 <goldstardev2002@gmail.com>
2022-01-24 00:13:35 +0900

    Trading View FUll Chart

---
+++ src/datafeed.js
@@ -0,0 +1,46 @@
+import { makeApiRequest, parseFullSymbol, generateSymbol } from './helper';
+// ...
+export default {
+  // ...
+    getBars: async (symbolInfo, resolution, periodParams, onHistoryCallback, onErrorCallback) => {
+        const { from, to, firstDataRequest } = periodParams;
+        console.log('[getBars]: Method call', symbolInfo, resolution, from, to);
+        const parsedSymbol = parseFullSymbol(symbolInfo.full_name);
+        const urlParameters = {
+            e: parsedSymbol.exchange,
+            fsym: parsedSymbol.fromSymbol,
+            tsym: parsedSymbol.toSymbol,
+            toTs: to,
+            limit: 2000,
+        };
+        const query = Object.keys(urlParameters)
+            .map(name => `${name}=${encodeURIComponent(urlParameters[name])}`)
+                .join('&');
+        try {
+            const data = await makeApiRequest(`data/histoday?${query}`);
+            if (data.Response && data.Response === 'Error' || data.Data.length === 0) {
+                // "noData" should be set if there is no data in the requested period.
+                onHistoryCallback([], { noData: true });
+                return;
+            }
+            let bars = [];
+            data.Data.forEach(bar => {
+                if (bar.time >= from && bar.time < to) {
+                    bars = [...bars, {
+                        time: bar.time * 1000,
+                        low: bar.low,
+                        high: bar.high,
+                        open: bar.open,
+                        close: bar.close,
+                    }];
+                }
+            });
+            console.log(`[getBars]: returned ${bars.length} bar(s)`);
+            onHistoryCallback(bars, { noData: false });
+        } catch (error) {
+            console.log('[getBars]: Get error', error);
+            onErrorCallback(error);
+        }
+    },
+//...
+};
